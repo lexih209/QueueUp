ec8473aaca0350ba2615b76215f1e35417d683c2
PJiggy <pjinocencio@gmail.com>
2025-11-28 23:56:15 -0800

    updated: added another variable to user class: isDummy in order to tell if user is a dummy profile or actual user;  added a question list in addition to the more compatible ones-users answer the questions and the bots randomly answer them; added add and print functions for the question list;

--- User.h
+++ User.h
@@ -3,22 +3,120 @@
 #define USER_H
 
 #include <string>
+#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <random>
 
 using namespace std;
+
+random_device rd; // too add the random answers for users
+mt19937 gen(rd()); // twister engine
+bernoulli_distribution dist(0.5); // 50% for true or false
 
 
 class User {
 
 public:
     string username, gender, favoriteGenre, schedule;
-    bool isCompetitive;
+    bool isCompetitive, isDummy;
 
-    vector<bool> quizAnswers;
+    // array for questions
+    vector<string> qList[5]; // vector index corresponds to points
+    // ex: ?array[1]["Do you like mansplaining?"] That question is one point
 
-    User(string name = "", string gen = "", string favGenre = "", string sched = "", bool competitive = false)
-        : username(name), gender(gen), favoriteGenre(favGenre), schedule(sched), isCompetitive(competitive) {}
+    // need a corresponding array for the users answers to each question
+    vector<bool> responses[5]; 
+    // *NOTE* Need to always alter both whenever removing a question
+
+    // for debugging purposes will make a print list function
+    void printQlist(){
+        cout << "Printing question list:\n";
+        for(int i = 0; i < 5; i++){
+            cout << "For " << i + 1 << "points: " << endl;
+            for(string question : qList[i]){
+                cout << question << endl;
+            }
+        }
+    }
+
+
+
+    // adds questions to make it easier for programming
+    // shouldn't be actually used once running, would have to ask all users!!! 
+    void addQuestion(int points, string question){ 
+        if(points > 5 || points <= 0){ // just error checking
+            cerr << "Error: Questions are only 1 to 5 points.";
+        }
+
+        // add question to list 
+        qList[points - 1].push_back(question); //(points -1) because array index
+        responses[points - 1].push_back(false); // just default to false
+        //printQlist();
+    }
+
+    // need to create a default constructor adds randoms answers for questions
+    User(string name, string sex, string genre, string date, bool comp, bool human){
+        if(human){ // means called for a user not a dummy profile
+            username = name;
+            gender = sex;
+            favoriteGenre = genre; 
+            schedule = date;
+            isCompetitive = comp;
+            isDummy = false;
+            return; 
+        }
+        cerr << "Error with creating this user profile.";
+    } 
+
+
+    void answerQuestions(){
+        // need to go through each index of the array and each question
+        int response;
+        // tell the user how to answer
+        cout << "For the following questions enter 0 for false and 1 for true.\n";
+
+        for (size_t p = 0; p < 5; ++p){ // outer loop(points)
+            // tell user how much these points are worth
+            cout << "\nThese questions are worth " << p + 1 << " point(s):\n";
+            for (size_t q = 0; q < qList[p].size(); ++q){ 
+            // go through all questions at this point value(inner loop)
+            // print out question for user
+                cout << qList[p][q] << endl;
+                // ask user for their response
+                cin >> response; 
+                // update user response
+                responses[p][q] = response; 
+            }
+        }
+    }
+
+    // fill in the questions for the dummy profiles
+    void fillQuestions(){
+        for (size_t p = 0; p < 5; ++p){ // outer loop(points)
+            for (size_t q = 0; q < qList[p].size(); ++q){ 
+            // go through all questions at this point value(inner loop)
+            bool randomBool = dist(gen); // generates true or false
+                responses[p][q] = randomBool; // puts random value for current
+            }
+        }
+    }
+
+    /* questions for future:
+     If were were to make a scale for the user to answer for each question,
+     how would we implement that? 
+     Would maybe have to change from boolean to int for the answers if, that's
+     the direction we want to go?
+    */
+    User(string name = "", string gen = "", string favGenre = "", string sched = "", bool competitive = false){
+        username = name;
+        gender = gen;
+        favoriteGenre = favGenre; 
+        schedule = sched;
+        isCompetitive = false;
+        isDummy = true;
+    }
+
 
 
     //Comparison logic
@@ -38,20 +136,25 @@ public:
 
     }
 
+    // calculates simularity score between users
+    // changed from "int" to "size_t" because apparently it's used for vectors
     int similarityScore(const User& other ) const {
         int score = 0;
-
-        if (gender == other.gender) score++;
-        if (favoriteGenre == other.favoriteGenre) score++;
-        if (schedule == other.schedule) score++;
-        if (isCompetitive == other.isCompetitive) score++;
+        // check if the non-negotioables match 
+        if(!isCompatibleWith(other)){
+            return 0; // return 0 since they're noncompatible
+        }
+        // now go through each question and add up score
+        for (size_t p = 0; p < 5; ++p){ //outter loop(point value of each question)
+            for (size_t r = 0; r < responses[p].size(); ++r){ // r is response #
+                if(responses[p][r] == other.responses[p][r]){
+                    score += static_cast<int>(p + 1); // add by point value plus 1(array things)
+                    /// can change to .at() for a more safe access, [] is faster
+                }
+            }
+        }
         return score;
-
     }
-        
-    
 };
-
-
 
 #endif
---
+++ main2.cpp
@@ -0,0 +1,224 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <limits>  
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include "User.h"
+
+using namespace std;
+
+
+// array for questions
+vector<string> qList[5]; // vector index corresponds to points
+// ex: ?array[1]["Do you like mansplaining?"] That question is one point
+
+
+
+//Function that sets the user name and adds them to the user list
+//Currently unused
+string setUserName(map<string, vector<bool>>& userList) {
+    string userName;
+
+    cout << "\nPlease enter your name: ";
+    getline(cin, userName);
+    userList[userName]; // creates key but, doesn't have values yet
+    return userName;
+}
+
+
+// Function to load users from a CSV file
+void loadUsers(vector<User>& users, const string& filename) {
+    ifstream file(filename);
+    string line;
+
+    if (!file.is_open()) {
+        cout << "Error: Could not open file: " << filename << endl;
+        return;
+    }
+
+    while (getline(file, line)) {
+
+        stringstream ss(line);
+        string userName, gender, genre, schedule, playstyle;
+
+        getline(ss, userName, ',');
+        getline(ss, gender, ',');
+        getline(ss, genre, ',');
+        getline(ss, schedule, ',');
+        getline(ss, playstyle, ',');
+
+        bool isCompetitive = (playstyle == "True" || playstyle == "1" || playstyle == "true");
+
+        //store in user var
+        users.push_back(User(userName, gender, genre, schedule, isCompetitive));
+
+        }
+    
+}
+
+
+
+// TO DO: userQuiz() 
+//  Layer 4: Add more answer choices
+//  Layer 5: Input validation
+
+
+User quiz(){
+    int input; // to be used for input for question responses
+    string name, gender, favoriteGenre, schedule;
+
+    // Debug: ensure user exists in the map
+    //if (userList.find(userName) == userList.end()) {
+    //    // If somehow missing, create entry
+    //    userList[userName];
+   // }
+
+    //Get quiz answers, Layer 2 expansion to more questions
+    cout << "Enter your name: ";
+    getline(cin, name);
+
+    cout << "\nHello " << name << "! Let's get you started!" << endl;
+
+    cout << "What gender would you prefer to game with? (Male, Female, Other): ";
+    getline(cin, gender);
+
+    cout << "What is your favorite game genre? (Cozy / Survival / Action / Horror / Simulation / Strategy): ";
+    getline(cin, favoriteGenre);
+
+    cout << "When are you usually available to game? (Mornings / Afternoons / Evenings / Weekends): ";
+    getline(cin, schedule);
+
+    // Final Question in original format
+    // IMPORTANT: When adding more questions keep track of index of each
+    // this question is index 0 for example
+
+    cout << "Are you looking for a more competitive or casual experience? (Enter 1 or 2)\n";
+
+    while (!(cin >> input) || (input != 1 && input != 2)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid input. Enter 1 (competitive) or 2 (casual): ";
+    }
+
+    bool isCompetitive = (input == 1);
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // clear buffer
+
+    return User(name, gender, favoriteGenre, schedule, isCompetitive, true); 
+}
+
+
+// TO DO: matchmaking() function that matches user to other users based on quiz answers
+//  Layer 2: Match based on number of matching answers
+//  Layer 3: Add more questions and randomize top results
+//  Layer 4: Add more users to match against (read from file?)
+//  Layer 5: Output matches to a file
+void matchMaker(const User& currentUser, const vector<User>& users) {
+    vector<pair<string, int>> matches;
+
+    for (const auto& other : users) {
+        if (other.username == currentUser.username) continue;
+        if (!currentUser.isCompatibleWith(other)) continue;
+
+        int score = currentUser.similarityScore(other);
+        matches.push_back({other.username, score});
+    }
+
+    if (matches.empty()) {
+        cout << "\nNo matches found.\n";
+        return;
+    }
+
+    sort(matches.begin(), matches.end(),
+         [](auto& a, auto& b) { return a.second > b.second; });
+
+    cout << "\nTop Matches:\n";
+    size_t limit = matches.size() < 3 ? matches.size() : 3;
+    for (size_t i = 0; i < limit; ++i) {
+        cout << matches[i].first << " (Score: " << matches[i].second << ")\n";
+    }
+}
+
+// questions update for every user
+void addQuestion(vector<User>& list, int points, string question){
+    for(User& current : list){
+        current.addQuestion(points, question);
+    }
+}
+
+// separated it from the addQuestions because I don't want it to answer after
+// each question. 
+void scoreCalc(vector<User>& list ){
+    for(User& current : list){
+        if(current.isDummy){
+            current.fillQuestions(); // fill questions for dummies
+        }
+        else if(!current.isDummy){
+            current.answerQuestions(); 
+        }
+        else{
+            cerr << "Error: This isn't a dummy or person?";
+        }
+    }
+}
+
+// make a function to answer all questions for user and dummies
+
+void fillQs(vector<User>& list){
+    for(User& current : list){
+        if(current.isDummy == true){
+            current.fillQuestions();
+        }
+        else if(current.isDummy == false){
+            current.answerQuestions();
+        }
+        else{
+            cerr << "Something went wrong...";
+        }
+    }
+}
+
+
+int main() {
+    vector<User> users; // each user has true/false responses for series of questions
+
+    
+    //Load existing users from file
+    //loadUsers(users, "users.csv");
+    
+    cout << "Welcome to QueueUp!" << endl;
+    cout << "A matchmaking service for gamers!" << endl;
+
+    //Calls Quiz function to get user info and store quiz answers
+    User currentUser = quiz();
+    users.push_back(currentUser);
+
+    // load the dummy users
+    loadUsers(users, "users.csv");
+    
+    // add questions before running matchmaker 
+    // HAS TO BE AFTER ALL USERS ARE LOADED IN
+    addQuestion(users, 5, "Are you a variety gamer?");
+    addQuestion(users, 4, "Do you play on console?");
+    addQuestion(users, 3, "Do you have a quality headset?");
+    addQuestion(users, 2, "Do you have kids?");
+    addQuestion(users, 3, "Do you listen to music while you play?");
+    addQuestion(users, 5, "Can you tolerate toxicity?");
+    addQuestion(users, 2, "Do you have a loud dog?");
+    addQuestion(users, 4, "Are you brainrot?");
+    addQuestion(users, 1, "Do you play outside of your listed hours?");
+
+
+    // answer questions for whole list
+    // HAS TO BE AFTER QUESTIONS ARE ADDED
+    fillQs(users);
+    
+    //Run matchmaker function
+    matchMaker(users[0], users);
+
+    // Exit message
+    cout << "\nThank you for using QueueUp! Happy gaming!" << endl;
+    return 0;
+}
